c12339bca22cbd090d505fe6db20370475b05d49
MerakiShubh <shubhamt.champ@gmail.com>
2024-08-08 20:20:51 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_DOMAIN,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Authorization,Content-Type',
+  })
+);
+console.log('Here is origin: ', process.env.FRONTEND_DOMAIN);
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+//routes import
+import userRouter from './routes/user.routes.js';
+import videoRouter from './routes/video.routes.js';
+import likeRouter from './routes/like.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import tweetRouter from './routes/tweet.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import playlistRouter from './routes/playlist.routes.js';
+import healthRouter from './routes/healthcheck.routes.js';
+
+//routes declaration
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/video', videoRouter);
+app.use('/api/v1/like', likeRouter);
+app.use('/api/v1/comment', commentRouter);
+app.use('/api/v1/tweet', tweetRouter);
+app.use('/api/v1/dashboard', dashboardRouter);
+app.use('/api/v1/subscription', subscriptionRouter);
+app.use('/api/v1/playlist', playlistRouter);
+app.use('/api/v1/healthcheck', healthRouter);
+
+export { app };
---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const DB_NAME = 'alttube';
+
+export const coverImg_upOptions = {
+  folder: 'YT/cover-image',
+  resource_type: 'image',
+};
+
+export const avatar_upOptions = {
+  folder: 'YT/avatar',
+  resource_type: 'image',
+};
+export const thumbnail_upOptions = {
+  folder: 'YT/thumbnail',
+  resource_type: 'image',
+};
+
+export const video_upOptions = {
+  folder: 'YT/video',
+  resource_type: 'video',
+  eager: [{ streaming_profile: 'full_hd', format: 'm3u8' }],
+  eager_async: true,
+  eager_notification_url: process.env.BACKEND_URL,
+  // eager_notification_url: 'https://videotube-backend-delta.vercel.app/',
+};
+console.log('Here is backend url:', process.env.BACKEND_URL);
